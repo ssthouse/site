12373863e3ab7e54b47130eff5ab8703daa90aaa
leungwensen <leungwensen@gmail.com>
2018-06-07 11:10:05 +0800

    add new member

--- site/zh-cn/data.js
+++ site/zh-cn/data.js
@@ -152,6 +152,7 @@ module.exports = {
         { name: '米法'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/mifa.jpg'     , weibo: ''                                 , github: 'https://github.com/paleface001' } ,
         { name: '芈乐'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/mile.jpg'     , weibo: ''                                 , github: 'https://github.com/weepy3641'   } ,
         { name: '完白'  , jobTitle: '设计师'     , avatar: '${assets}/image/members/wanbai.png'   , weibo: ''                                 , github: ''                               } ,
+        { name: '象数'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/xiangshu.png' , weibo: 'https://weibo.com/1707284737'     , github: 'https://github.com/lzxue'       } ,
         { name: '萧庆'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/xiaoqing.jpg' , weibo: ''                                 , github: 'https://github.com/dxq613'      } ,
         { name: '亦叶'  , jobTitle: '设计师'     , avatar: '${assets}/image/members/yiye.png'     , weibo: ''                                 , github: ''                               } ,
         { name: '有田'  , jobTitle: '工程师'     , avatar: '${assets}/image/members/youtian.jpg'  , weibo: 'https://weibo.com/u/1869365461'   , github: 'https://github.com/TomHuangCN'  } ,
