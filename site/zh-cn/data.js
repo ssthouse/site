81cacab67c71a276ec35dc043b8c6a1326640d0a
leungwensen <leungwensen@gmail.com>
2018-08-16 11:11:56 +0800

    chore(structure): remove gallery

--- site/zh-cn/data.js
+++ site/zh-cn/data.js
@@ -43,14 +43,14 @@ const data = {
                 // changelog: { hideFromNav: true, text: '${resource.translate.changelog}', href: '${base}zh-cn/f2/3.x/tutorial/changelog.html' },
             }
         },
-        gallery: {
-            name: '${resource.translate.gallery}',
-            href: '${base}zh-cn/gallery/index.html',
-            icon: '${assets}/image/icon/gallery.svg',
-            links: {
-                // publications: { text: '经典文献', href: '' },
-            }
-        },
+        // gallery: {
+        //     name: '${resource.translate.gallery}',
+        //     href: '${base}zh-cn/gallery/index.html',
+        //     icon: '${assets}/image/icon/gallery.svg',
+        //     links: {
+        //         // publications: { text: '经典文献', href: '' },
+        //     }
+        // },
         vis: {
             name: '${resource.translate.vis}',
             href: '${base}zh-cn/vis/index.html',
