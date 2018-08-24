49b883870d25d358e408d471498a11ebfed4c0da
simaQ <sima.zhang1990@gmail.com>
2018-08-24 09:29:48 +0800

    update demo

--- site/zh-cn/data.js
+++ site/zh-cn/data.js
@@ -39,8 +39,8 @@ const data = {
                 demo: { text: '${resource.translate.demo}', href: '${base}zh-cn/f2/3.x/demo/index.html' },
                 api: { text: '${resource.translate.api}', href: '${base}zh-cn/f2/3.x/api/index.html' },
                 tutorial: { text: '${resource.translate.tutorial}', href: '${base}zh-cn/f2/3.x/tutorial/index.html' },
-                history: { text: '${resource.translate.history}', href: '${base}zh-cn/f2/3.x/tutorial/history.html' },
                 English: { text: 'English', href: 'https://antv.gitbook.io/f2' },
+                history: { text: '${resource.translate.history}', href: '${base}zh-cn/f2/3.x/tutorial/history.html' },
                 // changelog: { hideFromNav: true, text: '${resource.translate.changelog}', href: '${base}zh-cn/f2/3.x/tutorial/changelog.html' },
             }
         },
